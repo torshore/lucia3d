b32478739f44baf84a1690d11ea5c58cb87e9c1b
torshore <toryshoreman@gmail.com>
2018-03-31 09:49:11 -0400

    Add Saturn Icon files

---
+++ app/components/PlanetIcon2.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './PlanetIcon2.scss';
+
+export default function PlanetIcon2() {
+    return <div className="planet-icon-2">
+        <svg
+            x="0px"
+            y="0px"
+            width="100px"
+            height="100px"
+            viewBox="0 0 100 100"
+            className="_svg"
+        >
+            <circle className="st4" cx="50" cy="50" r="35"/>
+            <circle className="st5" cx="50" cy="50" r="35"/>
+            <circle className="st5" cx="85" cy="10" r="5"/>
+            <path
+                className="st2"
+                d="M15,55.9c-6.8,
+                    4-8.8,10.14-8,
+                    14.5c3.4,
+                    7,24.2,
+                    3.8,
+                    50.6-7l2-0.8
+                    c24-10.8,
+                    38-24.6,34.4-32.2
+            	    c-5.3-6.1-22.8-4.6-12-5"
+            />
+        </svg>
+    </div>
+}
